18c4411eaf2fed9d3df9be942de8947e6a500efb
daigo-max <daidaitakano@gmail.com>
2021-03-26 20:09:06 +0900

    add blog.jsx & article

---
+++ src/Article.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Article = (props) => {
+    return (
+        <div>
+            <h2>Reactコンポーネントの使い方</h2>
+            <h2>Reactコンポーネントの使い方</h2>
+        </div>
+    )
+};
+
+export default Article;
